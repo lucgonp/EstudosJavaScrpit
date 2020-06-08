e2c52f330ab7ebfb5d0102b46f0ed0fc039819f3
lucgonp <lucgonp@hotmail.com>
2020-06-08 11:26:48 -0300

    NovoProjeto

---
+++ inicio.js
@@ -0,0 +1,59 @@
+// Criando o objeto que recebe nome e idade
+let obj = {
+    nome : "Lucas",
+    idade: 29
+}
+
+console.log(obj.nome); //Printando o nome
+console.log(obj.idade); //Printando a idade
+
+//Criando o primeiro Array
+const a1 = [1,2,3,4,5];
+
+console.log(a1[4]); //Printando o array pegando o item presente na quarta posição
+
+//Alterando o valor da posição 0 para "Alterado"
+a1[0] = "Alterado";
+
+console.log(a1[0]); //Printando o array pegando o item presente na primeira posição
+
+//Criando um array que cria arrys em suas posições
+const a2 = [
+    [1,2,3,4], //Posição 0
+    [5,6,7,8]  //Posição 1
+]
+
+console.log(a2[1]); //Printando o array pegando o item presente na segunda posição
+
+//Criando a constante de tempo
+const hoje = new Date;
+//Criando a constatnte de tempo com campos especificos
+const dt = new Date(2020,5);
+
+console.log(hoje); //Print da constante tempo
+console.log(dt); //Print da constante tempo
+
+let temp = 23; //Criando a variavel tempo
+let str = `Tempo em Goiania é de ${temp}`; //Criando a string 
+
+console.log(str);//Printando a variavel tempo junto com a mensagem usando ``
+
+let horario = 10; //Criando a variavel horário
+
+if(horario < 12){ //Validação se TRUE
+let teste;
+teste = obj;
+console.log(teste);
+console.log("Bom dia");
+console.log("Bem vindo ao trabalho");
+}
+else if(horario == 13){ //Validação se é exato
+console.log("Horario do cafe");
+}else{ //Validação se FALSE
+    console.log("Boa tarde");
+}
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,57 @@
+let opcao = 5;
+let cont = 9;
+
+switch (opcao) {
+    case 1:
+        if (cont < 10) {
+            do {
+                cont++;
+                console.log("valor apresentado é: " + cont);
+            } while (cont < 10) {
+                console.log(cont);
+            }
+        } else {
+            console.log("valor invalido.");
+        };
+        break;
+
+    case 2:
+        if (cont < 10) {
+            console.log("valor é: " + cont);
+            if (cont == 5) {
+                console.log("valor estático");
+            }
+        } else {
+            console.log("valor muito alto");
+        };
+        break;
+
+    case 3:
+        let obj = {
+            nome: "Lucas",
+            idade: 29
+        }
+
+        console.log(obj.nome);
+        console.log(obj.idade);
+        break;
+
+    case 4:
+        let ar = [1, 2, 3, 4, 5];
+        console.log(ar[1]);
+        break;
+
+    case 5:
+        if (cont < 10) {
+            while(cont < 10){
+                console.log("o valor é: " + cont);
+                cont++;
+            }
+        } else {
+            console.log("Passou ou é igual a 10")
+        }    
+        break;
+  
+    default:
+        console.log("errado");
+}
