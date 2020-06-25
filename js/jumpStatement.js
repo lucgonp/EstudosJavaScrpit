c92f81556b1eb6fc5f6ff591dcf84379982bf497
lucgonp <lucgonp@hotmail.com>
2020-06-25 11:22:46 -0300

    detalhes

--- js/jumpStatement.js
+++ js/jumpStatement.js
@@ -32,22 +32,26 @@ s: {
 while (x < 10) {
     x++;
     if (x == 5) {
-        y = multiplicar();
+        y = multiplicar(); /** chamando a função Multiplicar */
         console.log(y);
         console.log("Aplicou a Função criada para Y");
         continue;
     }if (x == 7){
-        z = sorte();
+        z = sorte(); /** chamando a função sorte */
         continue;
     }
     console.log(x);
 }
+
+/** Declarando a função que vai realizar a multiplicação do valor de X por 2. */
 
 function multiplicar(y) {
     y = x;
     y *= 2;
     return y;
 }
+
+/** Criando a função que apresentará a mensagem "Numero da Sorte" quando for chamada. */
 
 function sorte(z){
     console.log("Numero da sorte")
