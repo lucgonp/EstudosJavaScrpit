e38cf485a42bfc058676f27dae3cad3fec8e2ea5
lucgonp <lucgonp@hotmail.com>
2020-07-01 11:01:11 -0300

    destructionAndCall

--- js/functions.js
+++ js/functions.js
@@ -28,6 +28,30 @@ const a2 = arg1 => { arg1 = a; return arg1; }
 console.log("Nessa opção passamos um argumento, onde o valor é: " + a2());
 
 //Para passar mais de um argumento é necessário utilizar ()
-const a3 = (arg1, arg2) => { arg1 = a; arg2 = b; return a * b; } 
+const a3 = (arg1, arg2) => { arg1 = a; arg2 = b; return a * b; }
 console.log("Nessa opção passamos dois argumentos, onde a multiplicação dos dois valores é igual a: " + a3());
 
+//Utilizando destruction com uma function
+const calc1 = {
+    num1: 29,
+    num2: 8
+}
+
+function getCalc({ num1, num2 }) {
+    return `A soma dos valores é igual a: ${num1 + num2}`;
+}
+
+console.log(getCalc(calc1));
+
+//Utilizando o call para receber informação
+function nome() {
+    var resposta = ['Me chamo ', this.meuNome, ' e tenho ', this.minhaIdade, ' anos!'].join('');
+    console.log(resposta);
+}
+
+const apresentacao = {
+    meuNome: "Lucas",
+    minhaIdade: 29
+}
+
+nome.call(apresentacao);
