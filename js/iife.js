c780486db6b614d24448a4305c0cd7bd83b80827
lucgonp <lucgonp@hotmail.com>
2020-07-10 11:19:49 -0300

    strict

--- js/iife.js
+++ js/iife.js
@@ -1,6 +1,11 @@
-{
-    (function () {
-        let x = "teste";
-        console.log(x);
-    })();
-}
+/** Utilizando o 'use strict' ajudaa a detectar erros no código mais facilmente
+ * Evitar usar palavras reservadas, subscrevendo funções do JavaScript, e que você use variáveis não declaradas.  */
+'use strict'
+
+let x;
+
+(function (){
+    x = 5 + 5;
+    return x;
+})();
+
