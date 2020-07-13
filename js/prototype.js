45e11c3f621cef3131a917ac0853d022b511f664
lucgonp <lucgonp@hotmail.com>
2020-07-13 10:52:09 -0300

    herença

--- js/prototype.js
+++ js/prototype.js
@@ -1,5 +1,5 @@
 'use strict'
-
+/** Utilzando prototype */
 {
     let f = function () {
         this.a = 1;
@@ -11,15 +11,15 @@
     f.prototype.e = 5;
 
     console.log(o)
-    
-    
+
+
 
 }
 
 {
-    let nome = function(){
+    let nome = function () {
         this.nome = "Lucas";
-        
+
     }
 
     let sobrenome = new nome();
@@ -28,5 +28,22 @@
     let nomeCompleto = sobrenome;
     console.log(nomeCompleto);
 
-   
+
+}
+
+/** Herança */
+
+{
+    var f = {
+        a : 5,
+        soma : function (b) {
+            return this.a + 1;
+        }
+    }
+
+    console.log(f.soma());
+
+    var p = Object.create(f); /** Aqui estamos criando um novo objeto */
+    p.a = 77; /** Aqui o P herda oq foi declarado no objeto F */
+    console.log(p.soma());
 }
